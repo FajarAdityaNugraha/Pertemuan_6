5d223cab636869cb2318c1b33d59d954d1a8c352
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2023-10-12 13:30:34 +0700

    Pertemuan 6

---
+++ Pemilihan2Percobaan2No10.java
@@ -0,0 +1,27 @@
+
+/**
+ * Pemilihan2Percobaan2No10
+ */
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan2No10 {
+
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);
+        System.out.println("Masukkan sudut 1 : ");
+        float sudut1 = input10.nextInt();
+        System.out.println("Masukkan sudut 2 : ");
+        float sudut2 = input10.nextInt();
+        System.out.println("Masukkan sudut 3 : ");
+        float sudut3 = input10.nextInt();
+        float totalSudut = sudut1 + sudut2 + sudut3;
+        if (totalSudut == 180) {
+            if ((sudut1 == 90 || (sudut2 == 90) || (sudut3 == 90)))
+            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else
+            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        } else{
+            System.out.println("Bukan Segitiga");
+    }
+}
+}
