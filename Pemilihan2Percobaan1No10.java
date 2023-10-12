326a36f10081cb5959cf27d8248c1206eb05530c
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2023-10-12 14:17:09 +0700

    Pertemuan 6

--- Pemilihan2Percobaan1No10.java
+++ Pemilihan2Percobaan1No10.java
@@ -12,6 +12,9 @@ public class Pemilihan2Percobaan1No10 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
+             
+            else
+            System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
